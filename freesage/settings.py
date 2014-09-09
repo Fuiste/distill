ecfddaf4b67de34df2bceff3d81176f5de29ee81
Rudy <drawncloser@gmail.com>
2014-09-09 10:16:38 -0500

    More settings init

--- freesage/settings.py
+++ freesage/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'freesage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('MVP_DB_NAME', 'freesage_db'),
+        'USER': os.envirom.get('POSTGRES_DB_USER', 'fuiste'),
+        'PASSWORD': os.environ.get('POSTGRES_DB_PASS', ''),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
