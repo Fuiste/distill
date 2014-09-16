ca8c7e8dd57e237c883123a2a5d6a11475e5c0e8
Rudy <drawncloser@gmail.com>
2014-09-16 15:41:23 -0500

    Statics

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import os
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
 
 # Create your views here.
+
+class AppLandingView(View):
+    def get(self, request):
+        return render_to_response("app/ember_main.html", {}, context_instance=RequestContext(request))
