35a0e4adf1f6a549afb58d4b4cb480a2533100ec
Rudy <drawncloser@gmail.com>
2014-09-17 12:51:03 -0500

    Average scores tracked

--- static/js/app/explore/models.js
+++ static/js/app/explore/models.js
@@ -24,6 +24,28 @@ $(function(){
     reviews: DS.hasMany('review'),
     sortedReviews: function(){
       return this.get('reviews').sortBy('grade').reverse();
-    }.property('reviews')
+    }.property('reviews'),
+    averageScore: function(){
+      var score = 0;
+      var num = 0;
+      this.get('reviews').forEach(function(r){
+        score += r.get('grade');
+        num++;
+      });
+      score = score / num;
+      return score;
+    }.property('@each.reviews'),
+    averageScoreHtml: function(){
+      var score = this.get('averageScore');
+      var html = "";
+      while(score>0.5){
+        html += "<i class='fa fa-star'></i> ";
+        score--;
+      }
+      if(score>0){
+        html += "<i class='fa fa-star-half'></i> ";
+      }
+      return html;
+    }.property('averageScore')
   });
 });
