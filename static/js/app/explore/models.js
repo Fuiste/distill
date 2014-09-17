5f2ca1affb842015cf619a55b50218beb0f125b9
Rudy <drawncloser@gmail.com>
2014-09-17 12:09:45 -0500

    Sorting and styling, yo

--- static/js/app/explore/models.js
+++ static/js/app/explore/models.js
@@ -7,11 +7,23 @@ $(function(){
   //Models
   e.Review = m.extend({
     text: a('string'),
-    grade: a('number')
+    grade: a('number'),
+    htmlGrade: function(){
+      var grade = this.get('grade');
+      var html = "";
+      while(grade>0){
+        html += "<i class='fa fa-star'></i> ";
+        grade--;
+      }
+      return html;
+    }.property('grade')
   });
 
   e.Property = m.extend({
     name: a('string'),
-    reviews: DS.hasMany('review')
+    reviews: DS.hasMany('review'),
+    sortedReviews: function(){
+      return this.get('reviews').sortBy('grade').reverse();
+    }.property('reviews')
   });
 });
