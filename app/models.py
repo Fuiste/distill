cbb65aa73f7e0a8a4667ce81a5a005700775fca2
Rudy <drawncloser@gmail.com>
2014-09-18 10:15:28 -0500

    Actually using the DB

--- app/models.py
+++ app/models.py
@@ -8,6 +8,9 @@ class Review(models.Model):
     text = models.CharField(max_length=5000)
     grade = models.IntegerField(null=True)
 
+    def get_ember_dict(self):
+        return {"text": self.text, "grade": self.grade, "id": self.id}
+
 
 class Property(models.Model):
     """
@@ -16,4 +19,10 @@ class Property(models.Model):
     #  Instance fields
     name = models.CharField(max_length=100)
     reviews = models.ManyToManyField(Review, null=True)
+  
+    def get_ember_dict(self):
+        return {"name": self.name, "id": self.id, "reviews": [r.id for r in self.reviews.all()]}
+
+    def get_all_review_dicts_for_ember(self):
+        return [r.get_ember_dict() for r in self.reviews.all()]
 
--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
+from app.models import *
 
 # Create your views here.
 
@@ -16,18 +17,13 @@ class AppLandingView(View):
         return render_to_response("app/ember_main.html", {}, context_instance=RequestContext(request))
 
     def post(self, request):
-        return HttpResponse(json.dumps({"property_id": 1}), content_type="application/json")
+        prop = Property.objects.get(name="Ian's Pizza on State (fake)")
+        return HttpResponse(json.dumps({"property_id": prop.id}), content_type="application/json")
 
 class PropertiesView(View):
 
     def get(self, request, property_id):
         """Returns dummy data for now
         """
-        prop = {"name": "Test restaurant", "id": 1, "reviews": [1, 2, 3, 4, 5]}
-        reviews = []
-        reviews.append({"text": "This place is amazing - the food, service, atmosphere, everything is done well.  They've such carefully crafted and complex tasting pizzas.  They've high quality ingredients, the pizzas served hot, and even their salads are good.  Their staff is always friendly, greets you at the door, and certainly knows their pizza.  I love their wall art, too I'm a well known carnivore and I crave these guys' salad pizza with artichokes, olives, tomatoes, and salad (don't get me wrong, I still get a smokey bandit or BBQ tot...but I rave about a salad pizza to my friends and they're stunned to hear me endorse something vegetarian).  Definitely one of the places I'll miss the most moving away :(", "id": 1, "grade": 5})
-        reviews.append({"text": "Ever since my early visits in Madison I wanted to try Ian's.  Somehow we always ran out of time doing family stuff, but this time we got to pick up some pizzas...  Mac and Cheese, Florentine and Steak with barbecue sauce were really great  I was not hugely in on the crust, although it is ok, but I very much like the flavors on the toppings and the staff were great!!  I look forward to trying more pizza as my grandson is going to be born this week and if he has our family taste for pizza, we must continue the tradition....", "id": 2, "grade": 3})
-        reviews.append({"text": "Not sure what all the hoopla is about this pizza, it all tastes bland to me. Doesn't matter if you get the mac and cheese pie or a roasted red pepper and feta pie, the crust is so bland and the sauce/cheese lacks any flavor at all.Salad bar is 5 stars btw", "id": 3, "grade": 2})
-        reviews.append({"text": "I'd say Ian's is recognized more for their unconventional pizza ideas than the actual quality of the pizza. I got the mac and cheese pizza which was pretty much, um.. mac and cheese.. on pizza (try not to envy my way with words). I actually expected it to be a little cheesier though, but it was pretty flat. There was a burrito pizza that I was almost tempted to try, had I not glanced a tiny hair on one of the slices.  In my opinion, definitely worth a try but nothing to write home about.", "id": 4, "grade": 3})
-        reviews.append({"text": "I've had good and meh experiences here.  The service is always friendly, but often seem kind of frazzled.  I don't get the mac and cheese pizza at ALL. I think it's super bland. Their vegetarian options are also usually a little one note -- some sort of blend of tomato/pesto/cheese flavor. I'd love more veggie-heavy but interesting dishes.  The main thing I've come to like is the Southwest Salad. I started an internship on the square so on Tuesdays I head in .. and nearly every week they've been out of something for the salad! I got it as a whole ONCE in the past 5 weeks or so. I told my co-worker how yummy it was, she went to try it out, they were out of everything, and she got another salad ... and hated it. Haha. Come on!", "id": 5, "grade": 4})
-        return HttpResponse(json.dumps({"properties": [prop], "reviews": reviews}), content_type="application/json")
+        prop = Property.objects.get(id=property_id)
+        return HttpResponse(json.dumps({"properties": [prop.get_ember_dict()], "reviews": prop.get_all_review_dicts_for_ember()}), content_type="application/json")
