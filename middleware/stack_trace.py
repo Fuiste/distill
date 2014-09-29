53e46cb8e5638e303c8dfd6826fd675201a3a5f1
Rudy <drawncloser@gmail.com>
2014-09-29 12:17:00 -0500

    LOTS of libs for parsing

---
+++ middleware/stack_trace.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "MDee"
+
+import traceback
+import sys
+
+
+class ProcessExceptionMiddleware(object):
+    def process_response(self, request, response):
+        if response.status_code != 200:
+            print '\n'.join(traceback.format_exception(*sys.exc_info()))
+        return response
