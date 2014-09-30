969b00f0842b4a7908edfd328218bd5085102e43
Rudy <drawncloser@gmail.com>
2014-09-30 09:27:22 -0500

    Styling

--- app/views.py
+++ app/views.py
@@ -42,34 +42,36 @@ class PropertiesView(View):
         """
         Returns ember-friendly dicts for a property and its associated reviews
         """
+        # Grab property
         prop = Property.objects.get(id=property_id)
-        # Force NGRAM topics for now
-        # Uncomment for speedup, now it just runs all the time.
-        #generate = True
-        #if len(prop.topics.all()):
-        #    if prop.topics.all().category == 'NGRAM':
-        #        generate = False
-        #if generate:
-        #    find_and_init_ngrams_for_property(prop)
-        # review_date_cutoff = 2011
-        # yelp_spider = YelpSpider(url=prop.yelp_url, property_id=prop.id, provider_name="Yelp", review_date_cutoff=review_date_cutoff)
-        # print "Starting Yelp spider for {0}".format(prop.name)
-        # yelp_spider.start()
-        # print "Yelp done!"
-        #docs = []
-        #for r in prop.reviews.all():
-        #    docs.append({"text": r.text, "id": r.id})
-        #noun_phrase_list = pos_tag_text_documents(docs)
-        #prop.topics.all().delete()
-        #for n in noun_phrase_list[:10]:
-        #    new_topic = Topic(name=n["noun_phrase"], category='NOUNPHRASE')
-        #    new_topic.save()
-        #    for rid in n["ids"]:
-        #        new_topic.reviews.add(Review.objects.get(id=rid))
-        #    new_topic.save()
-        #    prop.topics.add(new_topic)
-        #    prop.save()
+
+        # If there's no reviews yet (initial GET) grab 'em
+        if len(prop.reviews.all()) == 0:
+            review_date_cutoff = 2011
+            yelp_spider = YelpSpider(url=prop.yelp_url, property_id=prop.id, provider_name="Yelp", review_date_cutoff=review_date_cutoff)
+            print "Starting Yelp spider for {0}".format(prop.name)
+            yelp_spider.start()
+            print "Yelp done!"
+
+        # If there's no topics yet, build them (now using NOUNPHRASE).
+        if len(prop.topics.all()) == 0:
+            docs = []
+            for r in prop.reviews.all():
+                docs.append({"text": r.text, "id": r.id})
+            noun_phrase_list = pos_tag_text_documents(docs)
+            prop.topics.all().delete()
+            for n in noun_phrase_list[:10]:
+                new_topic = Topic(name=n["noun_phrase"], category='NOUNPHRASE')
+                new_topic.save()
+                for rid in n["ids"]:
+                    new_topic.reviews.add(Review.objects.get(id=rid))
+                new_topic.save()
+                prop.topics.add(new_topic)
+                prop.save()
+    
+        # Uncomment to use NGRAM topics instead
         # find_and_init_ngrams_for_property(prop)
+
         return HttpResponse(json.dumps({"properties": [prop.get_ember_dict()], "reviews": prop.get_all_review_dicts_for_ember(), "topics": prop.get_all_topic_dicts_for_ember()}), content_type="application/json")
 
     @csrf_exempt
