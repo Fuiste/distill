0654ecc51b2e19c0704c0d3ca6631f06c41f8f64
Rudy <drawncloser@gmail.com>
2014-10-07 13:43:59 -0500

    Settings tweak

--- settings.py
+++ settings.py
@@ -85,7 +85,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
 
 STATIC_ROOT = "staticfiles"
 
